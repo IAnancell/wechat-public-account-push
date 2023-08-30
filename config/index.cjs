c8055902477450cd507c04455e0c82783b7549d8
SiMoe <64523151+IAnancell@users.noreply.github.com>
2023-08-31 07:58:52 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -20,30 +20,30 @@ USE_PASSAGE: 'push-deer',
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '晴晴宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU470TxmfUV6wb8nf7N14xUmQDlaomaABnMdu8',
+      id: 'PDU470TxmfUV6wb8nf7N14xUmQDlaomaABnMdu8','PDU25164TmXFnALUCVuB32gyVYoSb1SooFLcoydTO'
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '08-12',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '晴晴阴历生日', year: '2004', date: '06-27',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '晴晴阳历生日', year: '2004', date: '08-12',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '在一起的纪念日', year: '2023', date: '08-12',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2023-08-12' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
@@ -52,13 +52,13 @@ USE_PASSAGE: 'push-deer',
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '0002',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'PDU25164TmXFnALUCVuB32gyVYoSb1SooFLcoydTO','PDU470TxmfUV6wb8nf7N14xUmQDlaomaABnMdu8'
     }
   ],
 
