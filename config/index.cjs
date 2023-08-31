694fe94f7eb4250a427483ca35866e3f971fdc33
SiMoe <64523151+IAnancell@users.noreply.github.com>
2023-08-31 08:23:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ USE_PASSAGE: 'push-deer',
       // 想要发送的人的名字
       name: '晴晴宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU25164TmXFnALUCVuB32gyVYoSb1SooFLcoydTO','PDU470TxmfUV6wb8nf7N14xUmQDlaomaABnMdu8'
+      id: 'PDU470TxmfUV6wb8nf7N14xUmQDlaomaABnMdu8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
